def9a426398193d463f3d84d7869da79d071b1e5
Gurman-crypto <gurmandeep94@gmail.com>
2021-03-17 18:38:44 -0400

    first commint with pom xml dependencies

---
+++ DeliveryRobot/src/main/java/com/delivery/main/MainApp.java
@@ -0,0 +1,14 @@
+package com.delivery.main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+    System.out.println("Starting Delivery Robot Project!!");
+    
+	}
+
+}
